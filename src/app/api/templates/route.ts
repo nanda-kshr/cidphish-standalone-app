6780cf20ca20d063f7b933cd62abc937253655b4
nanda-kshr <nandakishorep212@gmail.com>
2025-04-21 20:56:55 +0530

    inititla

--- src/app/api/templates/route.ts
+++ src/app/api/templates/route.ts
@@ -14,12 +14,3 @@ export async function GET() {
     );
   }
 }
-
-
-export async function getFileContents(filePath: string): Promise<string> {
-  try {
-    return await fs.readFile(filePath, 'utf8');
-  } catch (err) {
-    throw new Error('Error reading the file: ' + (err as Error).message);
-  }
-}
